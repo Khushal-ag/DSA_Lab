c4563c2a1ad1cb46b909e80a5ca57100fa078038
Khushal_Agarwal <starkhush5@gmail.com>
2022-01-20 00:36:23 +0530

    Initial commit

---
+++ src/questions/Arrays/MeetingTime.java
@@ -0,0 +1,54 @@
+/*
+In GLA University there is various department. It is asked to every dept to conduct
+meeting for all the employees of GLA University. The central Management asked the
+dept kindly share starting time and ending time of the meeting in 24 hours format
+so that after verifying it will be circulated among the GLA employees.
+But there is a problem that before circulating the meeting information, the central
+management will find out if there are any clashes between time or not. If there are
+clashes it means the employee cannot attend all the meetings. Can you help central
+management to find it out?
+
+Use this class structure :-
+public class TimeInterval
+{
+    int start;
+    int end;
+    // use constructor, getter and setter methods as per requirement
+    public boolean canAttendMeeting(TimeInterval [ ] tlarr)
+    {
+    // set of statements
+    }
+}*/
+package questions.Arrays;
+
+class TimeInterval {
+    int start;
+    int end;
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public boolean canAttendMeeting(TimeInterval [ ] tlarr)
+    {
+        // set of statements
+    }
+}
+
+public class MeetingTime {
+    public static void main(String[] args) {
+
+    }
+}
