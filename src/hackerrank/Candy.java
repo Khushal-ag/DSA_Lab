fe6abd0f637e3c7504912bc92303e0a03b221b5c
Khushal_Agarwal <starkhush5@gmail.com>
2022-01-27 23:15:22 +0530

    Initial commit

--- src/hackerrank/Candy.java
+++ src/hackerrank/Candy.java
@@ -1,15 +1,10 @@
 package hackerrank;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static java.util.stream.Collectors.toList;
 
 class Result {
