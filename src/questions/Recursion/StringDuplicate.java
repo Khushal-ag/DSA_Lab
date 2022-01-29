b16d28d9bb2fe3bcda72f45b39241a1360bd3909
Khushal_Agarwal <starkhush5@gmail.com>
2022-01-29 14:02:14 +0530

    Initial commit

--- src/questions/Recursion/StringDuplicate.java
+++ src/questions/Recursion/StringDuplicate.java
@@ -1,13 +1,28 @@
 /*
 In a string S, remove consecutive duplicates from it recursively.
 
-        Sample Input 1 :
-        aabccba
-        Sample Output 1 :
-        abcba
+Sample Input 1 :
+aabccba
+Sample Output 1 :
+abcba
 */
 
 package questions.Recursion;
 
+import java.util.Scanner;
+
 public class StringDuplicate {
+    static String res = "";
+    public static String dup(String s,int currentIndex)
+    {
+        if(currentIndex==s.length()) return res;
+        if(s.charAt(currentIndex)!=s.charAt(currentIndex-1)) res += s.charAt(currentIndex);
+        return dup(s, currentIndex+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        res += s.charAt(0);
+        System.out.println(dup(s,1));
+    }
 }
