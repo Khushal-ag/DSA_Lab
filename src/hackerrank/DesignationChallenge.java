d1f74db076a61a55f204725f9387643681adb9ae
Khushal_Agarwal <starkhush5@gmail.com>
2022-01-29 13:29:49 +0530

    Initial commit

---
+++ src/hackerrank/DesignationChallenge.java
@@ -0,0 +1,27 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DesignationChallenge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<String> l = new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            l.add(sc.nextLine().toUpperCase(Locale.ROOT));
+            if(l.get(i).contains("HR")){
+                c++;
+                res.add(l.get(i));
+            }
+        }
+        System.out.println(c);
+        for (var i:res) {
+            System.out.println(i);
+        }
+    }
+}
