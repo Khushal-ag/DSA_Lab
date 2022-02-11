a02e78f5d0d75ca89631ee730aee8b910c10ff3b
Khushal_Agarwal <starkhush5@gmail.com>
2022-02-11 23:56:36 +0530

    Initial commit

--- src/questions/Arrays/RearrangeEvenOdd.java
+++ src/questions/Arrays/RearrangeEvenOdd.java
@@ -1,4 +1,14 @@
-package questions.Arrays;
+/*
+Print an array that contains the exact same number as the given array,
+but rearrange so that all the even numbers come before all the odd numbers.
+Other than that, the numbers can be in any order. You may modify and print
+the given array, or make a new array.
+
+Rearrange ([3,3,2]) -> [2,3,3]
+Rearrange(0,1,0,1,1,]) -> [0,0,1,1,1]
+*/
+
+        package questions.Arrays;
 
 import java.util.Scanner;
 
