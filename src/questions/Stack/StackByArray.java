098cf9df9ed34ebf9e2a3c0df517a50dc63a42be
Khushal_Agarwal <starkhush5@gmail.com>
2022-02-15 00:04:21 +0530

    Initial commit

---
+++ src/questions/Stack/StackByArray.java
@@ -0,0 +1,72 @@
+package questions.Stack;
+
+import java.util.Scanner;
+
+public class StackByArray {
+    int arr[];
+    int top;
+
+    public StackByArray(int size) {
+        arr = new int[size];
+        top = -1;
+    }
+    public boolean isEmpty()
+    {
+        if(top==-1) return true;
+        return false;
+    }
+    public boolean isOverflow()
+    {
+        if(top==arr.length - 1) return true;
+        return false;
+    }
+
+    public void push(int val)
+    {
+        if(isOverflow()) {
+            System.out.println("Stack is full");
+            return;
+        }
+        top++;
+        arr[top] = val;
+    }
+    public void pop()
+    {
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.println("Deleted element : "+arr[top]);
+        top--;
+    }
+    public void peep()
+    {
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+            return;
+        }
+        for(int i=0;i<=top;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        StackByArray s = new StackByArray(size);
+        s.push(12);
+        s.push(15);
+        s.peep();
+        s.push(19);
+        s.push(20);
+        s.peep();
+        s.pop();
+        s.pop();
+        s.peep();
+        s.pop();
+        s.pop();
+        s.pop();
+    }
+}
