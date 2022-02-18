b4691b6ba4ce94f05bedd2878e15d63c30e92324
Khushal_Agarwal <starkhush5@gmail.com>
2022-02-18 23:31:18 +0530

    Initial commit

---
+++ src/questions/project/EmployeeQueue/EmployeeMain.java
@@ -0,0 +1,28 @@
+/*
+Suppose there is an organization, which would like to propose work from home for their
+employees due to Covid-19 restrictions.An Employee of the organization could possibly
+choose to work from home, on a first-come, first-served, basis, only the first 'K'
+employees would be allowed to work from home. Now, observe the signature of the
+method below and complete the following method.
+
+public Queue allotWorkFrom Home (LinkedList employees, int k) {}
+
+The method receives a singly linked list of employee types as 'employees' and the
+number of employees who would be allowed to switch to work from home as 'k' as
+parameters. The method should take the first 'k' employees from the linked list,
+whose and put them into a queue and return the queue of employees. Only those
+to the queue whose 'workingFromHome' field is already set to true in the linked-list.
+
+public class Employee {
+    private String name;
+    private boolean workingFromHome;
+}
+*/
+
+package questions.project.EmployeeQueue;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+
+    }
+}
