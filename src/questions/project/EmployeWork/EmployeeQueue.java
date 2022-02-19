abd075cb86210fe08afe114ec26f290555d14303
Khushal_Agarwal <starkhush5@gmail.com>
2022-02-19 13:35:41 +0530

    Initial commit

--- src/questions/project/EmployeWork/EmployeeQueue.java
+++ src/questions/project/EmployeWork/EmployeeQueue.java
@@ -1,4 +1,33 @@
 package questions.project.EmployeWork;
 
+import questions.Queue.Implementation.QueueByList;
+
 public class EmployeeQueue {
+    Node front,rear;
+    class Node{
+        private Employee emp;
+        private Node next;
+
+        public Node(Employee emp) {
+            this.emp = emp;
+        }
+    }
+    public void enqueue(Employee emp)
+    {
+        Node node = new Node(emp);
+        if(rear==null && front==null){
+            front=rear=node;
+            return;
+        }
+        rear = rear.next = node;
+    }
+    public void display()
+    {
+        Node temp = front;
+        while (temp!=rear.next){
+            System.out.println( "Employee Name : "+temp.emp.getName() + " \t Work Allowance : "+temp.emp.isWorkingFromHome());
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
 }
