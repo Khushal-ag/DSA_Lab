abb0f1f9a5e5c67c65b016f6a0ee9fd23f40b276
Khushal_Agarwal <starkhush5@gmail.com>
2022-03-20 00:21:34 +0530

    Initial commit

---
+++ src/algorithm/sorting/insertionSort.java
@@ -0,0 +1,32 @@
+package algorithm.sorting;
+
+import java.util.Scanner;
+
+public class insertionSort {
+
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
