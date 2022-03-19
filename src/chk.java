ecce20ca354fb31646827e873e083fcc841a6b2c
Khushal_Agarwal <starkhush5@gmail.com>
2022-03-20 00:21:00 +0530

    Initial commit

---
+++ src/chk.java
@@ -0,0 +1,6 @@
+public class chk {
+    public static void main(String[] args) {
+        String s = new String("7");
+        System.out.println(1+102+s+1+10);
+    }
+}
--- src/questions/Stack/pallindrome.java
+++ src/questions/Stack/pallindrome.java
@@ -1,5 +1,7 @@
 package questions.Stack;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class pallindrome {
