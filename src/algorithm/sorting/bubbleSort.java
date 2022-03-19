9555993d715cbc47d45cece37972d62a969aa4b7
Khushal_Agarwal <starkhush5@gmail.com>
2022-03-20 00:21:23 +0530

    Initial commit

---
+++ src/algorithm/sorting/bubbleSort.java
@@ -0,0 +1,29 @@
+package algorithm.sorting;
+
+import java.util.Scanner;
+
+public class bubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
