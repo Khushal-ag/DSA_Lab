81bacb19e373e3dbdc595cfd7c9b99b23c576e1f
Khushal_Agarwal <starkhush5@gmail.com>
2022-03-20 00:32:41 +0530

    Initial commit

--- src/questions/LinkList/circular/insertion.java
+++ src/questions/LinkList/circular/insertion.java
@@ -22,6 +22,39 @@ public class insertion {
     public void insert(int val)
     {
         Node node = new Node(val,null);
+        if(head == null)
+        {
+            head = node;
+            tail = node;
+            tail.next = head;
+            return;
+        }
+        tail.next = node;
+        tail = node;
+        tail.next = head;
+    }
+    public void display()
+    {
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        do{
+            System.out.print(temp.val+" -> ");
+            temp = temp.next;
+        } while(temp!=head);
+        System.out.println(temp.val+" (HEAD) ");
+    }
 
+    public static void main(String[] args) {
+        insertion cq = new insertion();
+        cq.insert(21);
+        cq.insert(24);
+        cq.insert(25);
+        cq.display();
+        cq.insert(26);
+        cq.insert(30);
+        cq.display();
     }
 }
