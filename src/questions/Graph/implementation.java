510098fc962a614e41c083d0577c3dc1f65e65b2
Khushal_Agarwal <starkhush5@gmail.com>
2022-05-03 12:13:37 +0530

    initial commit

--- src/questions/Graph/implementation.java
+++ src/questions/Graph/implementation.java
@@ -1,2 +1,26 @@
-package questions.Graph;public class implementation {
+package questions.Graph;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class implementation {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number of vertices : ");
+            int n = sc.nextInt();
+            System.out.print("Enter the number of edges : ");
+            int m = sc.nextInt();
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                adj.add(new ArrayList<>());
+            }
+            for (int i = 0; i < m; i++) {
+                System.out.print("Enter the edge : ");
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+
+        }
 }
